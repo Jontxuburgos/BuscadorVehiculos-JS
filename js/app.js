294d76e6f247471dcf22579132a6aedbea6733b8
Jontxu <jonburgos4@gmail.com>
2023-11-16 16:25:49 +0100

    Insertar datos HTML y README

--- js/app.js
+++ js/app.js
@@ -8,5 +8,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //FUNCIONES
 function mostrarAutos() {
+    autos.forEach( auto => {
+        const { marca, modelo, year, puertas, transmision, precio, color} = auto;
+        const autoHTML = document.createElement('p');
 
+        autoHTML.textContent = `
+            ${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmisión ${transmision} - Precio ${precio} - Color ${color}
+        
+        
+        `;
+        //Insertar en HTML
+        resultado.appendChild(autoHTML)
+
+    } )
 };
